754bf391dc82efb997d8d77a15baddb80e2d8e58
Oleg Korolko <olegk101@gmail.com>
2016-09-06 10:40:12 +0300

    slider fixed

--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,7 @@ var Blog = require('../models/blogs').Blog;
 exports.get = function(req, res) {
   Blog.find({}, function(err, posts) {
     if (err) return next(err);
-    res.render('blog', {posts: posts});
+    res.render('blog', {posts: posts.reverse()});
   })
 };
 
